e28a9f0624265f57fadef8ccf2bdfefb6cd16a46
andreaeusebi <eusandre95@gmail.com>
2024-06-11 09:50:06 +0200

    Further improved lidar2camera code.

--- src/lidar2camera.cpp
+++ src/lidar2camera.cpp
@@ -23,10 +23,10 @@ int main(int argc, char **argv)
     }
 
     /* ----- Read input arguments ----- */
-    std::string image_dir   = argv[1];
+    std::string images_dir  = argv[1];
     std::string csv_file    = argv[2];
 
-    std::cout << "Given images directory: " << image_dir << std::endl;
+    std::cout << "Given images directory: " << images_dir << std::endl;
     std::cout << "Given CSV file path: "    << csv_file << std::endl;
 
     /* ----- Load lidar 3D center points from CSV ----- */
@@ -56,45 +56,53 @@ int main(int argc, char **argv)
     }
 
     /* ----- Load images from given directory ----- */
-    std::vector<cv::String> images;
-    cv::glob(image_dir, images);
+    std::vector<cv::String> image_paths;
+    cv::glob(images_dir, image_paths);
 
-    std::vector<cv::Mat> vec_mat;
+    std::vector<cv::Mat> images;
     std::vector<std::string> images_name;
 
     std::cout << "Found following images: " << std::endl;
-    for (const auto &path : images)
+    for (const auto &path : image_paths)
     {
         std::cout << path << std::endl;
         cv::Mat img = cv::imread(path, cv::IMREAD_GRAYSCALE);
-        vec_mat.push_back(img);
+        images.push_back(img);
         images_name.push_back(path);
     }
 
     /* ----- Set inputs to Camera Calibrator object ----- */
-    CameraCalibrator m;
-    cv::Mat camera_matrix = cv::Mat(3, 3, CV_32FC1, cv::Scalar::all(0));
-    cv::Mat k = cv::Mat(1, 5, CV_32FC1, cv::Scalar::all(0));
-    std::vector<cv::Mat> tvecsMat;
-    std::vector<cv::Mat> rvecsMat;
-    m.set_input(images_name, vec_mat, cv::Size{17, 7}, lidar_3d_pts);
+    cv::Mat camera_matrix    = cv::Mat(3, 3, CV_32FC1, cv::Scalar::all(0));
+    cv::Mat dist_coeffs      = cv::Mat(1, 5, CV_32FC1, cv::Scalar::all(0));
+    cv::Size chessboard_size (17, 7);
+    cv::Size image_size      (1920, 1080);
+
+    CameraCalibrator lidar_camera_calibrator;
+    lidar_camera_calibrator.set_input(images_name, images, chessboard_size, lidar_3d_pts);
 
     /* ----- Perform calibration and get results ----- */
-    m.get_result(camera_matrix, k, cv::Size{1920, 1080}, rvecsMat, tvecsMat);
+    std::vector<cv::Mat> rot_vectors;
+    std::vector<cv::Mat> trans_vectors;
+
+    lidar_camera_calibrator.get_result(camera_matrix,
+                                       dist_coeffs,
+                                       image_size,
+                                       rot_vectors,
+                                       trans_vectors);
 
     /* ----- Print results to terminal ----- */
     std::cout << "----- Calibration Completed! -----" << std::endl;
 
-    std::cout << "Camera Matrix:"             << std::endl << camera_matrix   << std::endl;
-    std::cout << "Dist coefficient:"          << std::endl << k               << std::endl;
-    std::cout << "Rotations vector size:"     << std::endl << rvecsMat.size() << std::endl;
-    std::cout << "Translations vector size:"  << std::endl << tvecsMat.size() << std::endl;
+    std::cout << "Camera Matrix:"             << std::endl << camera_matrix        << std::endl;
+    std::cout << "Dist coefficient:"          << std::endl << dist_coeffs          << std::endl;
+    std::cout << "Rotations vector size:"     << std::endl << rot_vectors.size()   << std::endl;
+    std::cout << "Translations vector size:"  << std::endl << trans_vectors.size() << std::endl;
 
-    std::cout << "Translation vector 0:"      << std::endl << tvecsMat[0] << std::endl;
-    std::cout << "Rotation vector 0:"         << std::endl << rvecsMat[0] << std::endl;
+    std::cout << "Translation vector 0:"      << std::endl << trans_vectors[0]     << std::endl;
+    std::cout << "Rotation vector 0:"         << std::endl << rot_vectors[0]       << std::endl;
     
-    std::cout << "Rotation vector 1:"         << std::endl << rvecsMat[1] << std::endl;
-    std::cout << "Translation vector 1:"      << std::endl << tvecsMat[1] << std::endl;
+    std::cout << "Rotation vector 1:"         << std::endl << rot_vectors[1]       << std::endl;
+    std::cout << "Translation vector 1:"      << std::endl << trans_vectors[1]     << std::endl;
 
     std::cout << "----------------------------------" << std::endl;
 
